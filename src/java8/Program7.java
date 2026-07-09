8c85022f4d4420fa27054734d91030c19ec5bb7d
mukeshkumar-bit <mukesh.kprd89@gmail.com>
2026-07-09 08:30:21 +0530

    add program7

--- src/java8/Program7.java
+++ src/java8/Program7.java
@@ -1,4 +1,16 @@
 package java8;
 
+import java.lang.management.ManagementPermission;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+//How do you find the frequency of each charcater in a string using java 8 stream api?
 public class Program7 {
+    public static void main(String[] args) {
+        String inputString="java Concept of the day";
+       java.util.Map<Character,Long> frequencyOfTheElement= inputString.chars().mapToObj(c->(char)c).collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new,Collectors.counting()));
+        System.out.println(frequencyOfTheElement);
+    }
 }
