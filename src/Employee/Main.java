6e9a64009dbc3e739b9616a5f33bae009bc183f2
mukeshkumar-bit <mukesh.kprd89@gmail.com>
2026-07-10 10:14:19 +0530

    10-july-2026

--- src/Employee/Main.java
+++ src/Employee/Main.java
@@ -2,6 +2,8 @@ package Employee;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
 	public static void main(String[] args) {
@@ -9,6 +11,17 @@ public class Main {
 		list.add(new Employee(111, "mukesh", 32, "Male", "HR", 2011, 2500.0));
 		list.add(new Employee(112, "rakesh", 31, "Male", "software", 2012, 2600.0));
 		list.add(new Employee(113, "saket", 34, "Male", "HR", 2011, 2800));
+		list.add(new Employee(114,"radha",60,"female","HR",2015,4000));
+		
+		
+		//How many mail and female employees are there in the organization
+		Map<String,Long> countMaleAndFemale=list.stream().collect(Collectors.groupingBy(Employee::getGender,Collectors.counting()));
+		System.out.println(countMaleAndFemale);
+		System.out.println("-----------------------------------------");
+		//print the name of all departments in the organization	
+		list.stream().map(Employee::getDepartment).distinct().forEach(System.out::println);
+		
+		
 
 	}
 
